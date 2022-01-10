af424e4c5224f1ba1dad57ad4a3d533c6d8a8b14
oeyoews <jyao4783@gmail.com>
2022-01-10 21:46:37 +0800

    doc: update(automatically)

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
-:: REM git pull
+:: git pull
 
 start /b tiddlywiki --listen port=8090  > .log.txt 2>&1
 
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" http://127.0.0.1:8090
+:: "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" http://127.0.0.1:8090
+start http://127.0.0.1:8090
