405c06bc21746b7b3b9944f3a31a079265c06da6
oeyoews <jyao4783@gmail.com>
2022-01-11 00:23:17 +0800

    doc: update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,17 @@
 :: git pull
 
-start /b tiddlywiki --listen port=8090  > .log.txt 2>&1
+:: start /b tiddlywiki --listen port=8090  > .log.txt 2>&1
 
 :: "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" http://127.0.0.1:8090
-start http://127.0.0.1:8090
+::
+:: start /b tiddlywiki --listen port=8090  > .log.txt 2>&1 && start http://127.0.0.1:8090
+
+@echo off
+echo tw is staruping
+
+start /b  tiddlywiki --listen port=8090  > .log.txt 2>&1
+start /MAX http://127.0.0.1:8090
+
+echo tw startup finished successfully
+
+pause
