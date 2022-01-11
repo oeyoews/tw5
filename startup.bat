6c01e0de4c438fafd6379b6fb8f7352c923b6f35
oeyoews <jyao4783@gmail.com>
2022-01-12 01:09:44 +0800

    doc: update(automatically)

--- startup.bat
+++ startup.bat
@@ -13,6 +13,7 @@ start /b  tiddlywiki --listen port=8090  > .log.txt 2>&1
 start /MAX http://127.0.0.1:8090
 
 echo tw startup finished successfully
-echo If close this window, this serve will be close.
+echo
+echo NOTE: If close this window, this serve will be close.
 
 REM pause
