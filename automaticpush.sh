f5c1f7b48ceb8252ecb262727a2a3c46b0f0da6f
oeyoews <jyao4783@gmail.com>
2022-03-03 20:45:01 +0800

    This is an crontab automatic push

--- automaticpush.sh
+++ automaticpush.sh
@@ -1,1 +1,3 @@
-git add . && git commit -m "This is an crontab automatic push" && git push
+#!/usr/bin/env bash
+#git add . && git commit -m "This is an crontab automatic push" && git push
+#make gitsync
