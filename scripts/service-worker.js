de15020e8f2804f6324b01dc1af1a64b854020c7
oeyoews <jyao4783@gmail.com>
2022-04-07 14:33:51 +0800

    🎣 This is an automatic push

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -40,6 +40,9 @@ registerRoute(
   })
 );
 
+registerRoute(/\.js$/, new StaleWhileRevalidate());
+registerRoute(/(^\/$|index.html)/, new StaleWhileRevalidate());
+
 //self test
 self.addEventListener('install', function(event) {
   // The promise that skipWaiting() returns can be safely ignored.
@@ -49,6 +52,3 @@ self.addEventListener('install', function(event) {
   // service worker to install, potentially inside
   // of event.waitUntil();
 });
-
-registerRoute(/\.js$/, new StaleWhileRevalidate());
-registerRoute(/(^\/$|index.html)/, new StaleWhileRevalidate());
