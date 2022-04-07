3290791b2d92a1100a6142857434ab55ebf2025d
oeyoews <jyao4783@gmail.com>
2022-04-07 14:23:34 +0800

    fix: fix service-worker can't refresh automaticly(even refresh by tip)

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -41,14 +41,14 @@ registerRoute(
 );
 
 //self test
-//self.addEventListener('install', function(event) {
-   //The promise that skipWaiting() returns can be safely ignored.
-  //self.skipWaiting();
+self.addEventListener('install', function(event) {
+  // The promise that skipWaiting() returns can be safely ignored.
+  self.skipWaiting();
 
-   //Perform any other actions required for your
-   //service worker to install, potentially inside
-   //of event.waitUntil();
-//});
+  // Perform any other actions required for your
+  // service worker to install, potentially inside
+  // of event.waitUntil();
+});
 
 registerRoute(/\.js$/, new StaleWhileRevalidate());
 registerRoute(/(^\/$|index.html)/, new StaleWhileRevalidate());
