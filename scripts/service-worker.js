81a3d96f887bfbeb17e092d4516669abb5b79a63
oeyoews <jyao4783@gmail.com>
2022-04-10 13:06:13 +0800

    fix: check install automatically

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -66,11 +66,19 @@ let deferredPrompt;
 
 self.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
   // Stash the event so it can be triggered later.
-  deferredPrompt = e;
   // Update UI notify the user they can install the PWA
-  showInstallPromotion();
   // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
+  //e.preventDefault();
+  //deferredPrompt = e;
+  //showInstallPromotion();
+  //console.log(`'beforeinstallprompt' event was fired.`);
+      e.userChoice.then(function (choiceResult) {
+        if (choiceResult.outcome === 'dismissed') {
+            console.log('用户取消安装应用');
+        }
+        else {
+            console.log('用户安装了应用');
+        }
+    });
 });
