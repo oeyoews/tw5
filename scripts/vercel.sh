0f036791656eca3063f495687c3ebd5266ea866b
oeyoews <jyao4783@gmail.com>
2022-04-17 12:29:34 +0800

    fix: remove dash for vercel.sh

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-yarn copy_manifest && yarn devbuild && yarn copy   && yarn copy_workbox && yarn workbox_inje
+yarn copymanifest && yarn devbuild && yarn copy   && yarn copyworkbox && yarn workboxinje
