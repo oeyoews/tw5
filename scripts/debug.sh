469bb30dad5dab813984d41daa99c5f976cb1ad9
oeyoews <jyao4783@gmail.com>
2022-04-25 17:50:28 +0800

    feat: remove debug sw

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -2,18 +2,18 @@
 rm -rf debug && mkdir debug
 echo 🎉 start: remove debug dir and creat debug dir
 
-# cp curren to temp no static
-cp -r assets scripts tiddlers  *.info *.json debug
+# cp curren to temp
+cp -r assets scripts tiddlers static *.info *.json debug
 echo 🐶 1. cp some folder
 
 cd debug
 echo 🔥 2. enter debug dire
 
 rm -rf tiddlers/subwiki
+rm -rf static/tidpatch/* touch static/tidpatch/fix
 echo 💊 3. remove subwiki contents
 
-# to fix no internet, use npm run
-npm run buildvercel
+yarn  buildvercel
 echo 🌹 end: start buildvercel
 
 echo 🚒 start open google-chrome-stable
