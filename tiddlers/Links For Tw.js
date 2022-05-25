5e4ee0b079a282b3afd28f1b6d57f06365c5953b
oeyoews <jyao4783@gmail.com>
2022-05-25 20:02:38 +0800

    fix: clean links for tw

---
+++ tiddlers/Links For Tw.js
@@ -0,0 +1,5 @@
+<link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Long+Cang&display=swap" rel="stylesheet">
+
+<script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
