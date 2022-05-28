ca583af6dedcb6a976176855d996027fe9566610
oeyoews <jyao4783@gmail.com>
2022-05-28 10:56:44 +0800

    feat: add tw-hitokoto fontawesome-v6

--- tiddlers/Links For Tw.js
+++ tiddlers/Links For Tw.js
@@ -3,5 +3,3 @@
 <link href="https://fonts.googleapis.com/css2?family=Long+Cang&display=swap" rel="stylesheet">
 
 <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet">
-	
-<script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
