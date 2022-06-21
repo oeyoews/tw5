ddd6e9f21afc1653d58819cdcd6fa32d7c9d0b94
oeyoews <jyao4783@gmail.com>
2022-06-21 22:29:31 +0800

    fix: remove some fonts

--- tiddlers/Links For Tw.js
+++ tiddlers/Links For Tw.js
@@ -1,7 +1,1 @@
-<link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
-
 <link href="https://fonts.googleapis.com/css2?family=Long+Cang&display=swap" rel="stylesheet">
-
-<link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet">
-	
-<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Patrick+Hand&display=swap" rel="stylesheet">
